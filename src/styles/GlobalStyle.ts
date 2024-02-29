bdbec1b494fc9621311140baa008263ed776b633
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-02-28 21:29:43 -0300

    :lipstick: feat: Adicionando fonte global

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -2,9 +2,13 @@
 import { createGlobalStyle } from 'styled-components';
  
 const GlobalStyle = createGlobalStyle`
-  body {
-    background-color: ${props => props.theme.background};
-  }
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
+    body {
+        background-color: ${props => props.theme.background};
+        font-family: "Poppins", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+    }
 `;
  
 export default GlobalStyle;
