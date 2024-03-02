fda8f7453511916cd8b0e3af193659eadf80c057
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-03-02 14:30:16 -0300

    :sparkles: feat: Adicionando menu lateral

--- src/components/AsideMenu/index.tsx
+++ src/components/AsideMenu/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import { MenuNav, MenuWrapper } from './style'
+import { MenuNav, MenuWrapper,MenuItem } from './style'
 import useMenu from '../../hooks/useMenu';
 
 export default function AsideMenu() {
   const {open, setOpen} = useMenu();
   return (
-    <MenuWrapper openMenu={open}>
-        <MenuNav >
-            menu
+    <MenuWrapper openMenu={open} onClick={()=> setOpen(false)}>
+        <MenuNav  onClick={(e) => e.stopPropagation()}>
+            <ul>
+              <li><MenuItem href='#'>Meu Vivo empresas</MenuItem></li>
+              <li><MenuItem href='#'>Meu Vivo</MenuItem></li>
+            </ul>
         </MenuNav>
     </MenuWrapper>
 
--- src/components/AsideMenu/style.tsx
+++ src/components/AsideMenu/style.tsx
@@ -5,14 +5,14 @@ type MenuNavProp =
     openMenu:boolean
 }
 export const MenuNav = styled.nav`
-    width: 420px;
+    width: 41.791vw;
     max-width: 420px;
     height: 100%;
-    padding:15px;
+    padding:51px 15px;
     position: relative;
     left: 0;
     background-color: ${props=> props.theme.background};
-    transition: transform 0.5s linear;
+    transition: all 0.6s linear;
 `
 export const MenuWrapper = styled.div<MenuNavProp>`
     height: 100%;
@@ -21,12 +21,32 @@ export const MenuWrapper = styled.div<MenuNavProp>`
     z-index: 99;
     left: 0;
     background: #6C6868C9;
+    opacity: ${props => props.openMenu ? "1" : "0"};
+    visibility: ${props => props.openMenu ? "visible" : "hidden"};;
+    transition: opacity 0.5s linear, visibility 0.5s linear;
     & ${MenuNav}
     {
         transform: translateX(${props => props.openMenu ? "0%" : "-100%"});
     }
-    opacity: ${props => props.openMenu ? "1" : "0"};
-    visibility: ${props => props.openMenu ? "visible" : "hidden"};;
-    transition: opacity 0.5s linear, visibility 0.5s linear;
     
+`
+
+export const MenuItem = styled.a`
+    margin-bottom: 21px;
+    display: block;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 30px;
+    letter-spacing: 0em;
+    text-align: left;
+    color: black;
+    border-bottom: 1px solid ${props=>props.theme.primary+"7F"};
+    text-decoration: none;
+    transition: all 0.4s linear;
+
+    &:hover
+    {
+        color: ${props=>props.theme.primary};
+    }
+
 `
