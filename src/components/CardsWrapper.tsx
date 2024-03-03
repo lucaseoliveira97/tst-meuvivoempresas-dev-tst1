51f970e1855617ef35c08e2fd09d8b47b1370d8b
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-03-03 11:12:08 -0300

    :recycle: refactor: Ajustes para responsividade dos cards

--- src/components/CardsWrapper.tsx
+++ src/components/CardsWrapper.tsx
@@ -6,12 +6,17 @@ export const CardsWrapper = styled.div`
     justify-content: space-between;
     align-items: center;
     height: 187px;
-    padding: 0 4.469rem;
-    gap: 26px 18px;
+    padding: 0 clamp(0rem, -6.6207rem + 17.6553vw, 4.469rem);;
+    margin: 1.813rem 0;
+    gap: 1.625rem 1.125rem;
     width: 100%;
-    @media (max-width: ${({theme}) => theme.mobileBreakpoint}px) {
+    @media (max-width: ${({theme}) => theme.smBreakpoint}px) {
         flex-direction: column;
-        padding: 0 10px;
+        padding: 0%;
+        gap: 46px 18px;
+  }
+  @media (min-width:${({theme}) => theme.smBreakpoint+1}px) and (max-width: ${({theme}) => theme.mdBreakpoint}px) {
+        background-color: red;
   }
 `
 
