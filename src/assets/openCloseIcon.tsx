e58f657d2c117fc590f84a35b762ffdbf1ee1c98
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-03-03 12:42:06 -0300

    :sparkles: feat: Novo icone de abrir e fechar menu

---
+++ src/assets/openCloseIcon.tsx
@@ -0,0 +1,49 @@
+import { styled } from "styled-components";
+
+type OpenCloseIconProps = {
+ open:boolean,
+ onClick: ()=>void
+}
+
+type IconLineProps =  {
+    open:boolean,
+}
+export const IconLine = styled.div`
+    height: 3px;
+    background-color: ${props=> props.theme.primary};
+    border-radius: 1px;
+    transition: .2s;
+
+`
+export const IconWrapper = styled.div<IconLineProps>`
+   display: flex;
+    flex-direction: column;
+    row-gap: 5px;
+    width: 32px;
+    cursor: pointer;
+    &:hover ${IconLine}
+    {
+        background-color: ${props=>props.theme.secondary};
+    }
+    & ${IconLine}:first-child {
+        transform: rotate(${props=>props.open? "40deg" : "0deg"}) translate(${props=>props.open? "5px, 6.45px" : "0"});
+    }
+    & ${IconLine}:nth-child(2) {
+        opacity: ${props=>props.open? "0" : "a"};
+    }
+    & ${IconLine}:last-child {
+        transform: rotate(${props=>props.open? "-40deg" : "0deg"}) translate(${props=>props.open? "5px,-6.45px" : "0"})
+    }
+`
+
+const OpenCloseIcon =({ open,onClick }: OpenCloseIconProps) =>{
+    return (
+        <IconWrapper onClick={onClick} open={open}>
+            <IconLine />
+            <IconLine />
+            <IconLine />
+        </IconWrapper>
+    );
+}
+
+export default OpenCloseIcon
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,13 +3,16 @@ import menuIcon from '../../assets/menu-icon.svg'
 import closeMenuIcon from '../../assets/close-icon.svg'
 import vivoLogo from '../../assets/vivo-logo-1.png'
 import useMenu from "../../hooks/useMenu"
+import OpenCloseIcon from "../../assets/openCloseIcon"
 
 const Header =() =>{
     const {open, setOpen} = useMenu();
     return (
         <HeaderContainer>
             <HeaderWrapper>
-                <MenuIconWrapper><MenuIcon src={open ? menuIcon:closeMenuIcon} alt="Menu icon" onClick={() => setOpen(prev=>!prev)}/></MenuIconWrapper>
+                <MenuIconWrapper>
+                    <OpenCloseIcon open={open} onClick={() => setOpen(prev=>!prev)} />
+                </MenuIconWrapper>
                 <VivoLogo src={vivoLogo} alt="Vivo logotipo" />
             </HeaderWrapper>
         </HeaderContainer>
